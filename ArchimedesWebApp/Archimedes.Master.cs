b67967a368a9ca365d5e41daee75ce8c9437ec69
ArchimedesTeamLeader <lricha4295@students.pcci.edu>
2015-09-26 16:44:55 -0500

    tried to fix the HEAD tag

--- ArchimedesWebApp/Archimedes.Master.cs
+++ ArchimedesWebApp/Archimedes.Master.cs
@@ -13,5 +13,10 @@ namespace ArchimedesWebApp
         {
 
         }
+
+        protected void btnHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/CEODashboard.aspx");
+        }
     }
 }
